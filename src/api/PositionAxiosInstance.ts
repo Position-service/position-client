2a337335e551a21a9bdf1ecc22ee9122e118f4ce
hyemmie <lhm0710@naver.com>
2021-07-08 17:31:58 +0900

    feat axiosinstance

---
+++ src/api/PositionAxiosInstance.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import SignInStatus, { SignOut } from '../enums/SignInStatus';
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+export const authenticatedClient = (
+  autoSignOutHandler: (signInStatus: SignOut) => void,
+  token: string,
+) => {
+  const axiosInstance = axios.create({
+    baseURL,
+    headers: { authorization: token },
+  });
+
+  axiosInstance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (e) => {
+      if (e.response.status === 401) {
+        autoSignOutHandler(SignInStatus.EXPIRED);
+      } else if (e.response.status === 500) {
+        alert("서버 오류입니다.");
+      }
+      return Promise.reject(e);
+    },
+  );
+  return axiosInstance;
+};
+
+export const client = axios.create({
+  baseURL,
+});
