638201671db1e7d9b8405b431b0cdd55b9c07759
hyemmie <lhm0710@naver.com>
2021-07-14 11:27:07 +0900

    feat login view

--- src/modules/account/LoginTemplate.tsx
+++ src/modules/account/LoginTemplate.tsx
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './css/Login.css';
-import logo1x from './img/position-logo.png';
-import logo2x from './img/position-logo@2x.png';
 
 interface Props {
-  loginHandler: (username: string, password: string) => void;
+  loginHandler: (email: string, password: string) => void;
   signupHandler: (
     username: string,
     password: string,
-    passwordCheck: string
+    passwordCheck: string,
+    nickName: string
   ) => void;
 }
 
+interface State {
+  email: string;
+  password: string;
+}
+
 export const LoginTemplate: React.FunctionComponent<Props> = (props: Props) => {
+  const [loginInfo, setLoginInfo] = useState<State>({
+    email: '',
+    password: '',
+  });
+
   return (
     <section className="section-main">
       <div className="circle-wrapper">
         <div className="circle-green">
-          <div className="position-logo">
-            <img
-              src={logo1x}
-              srcSet={logo1x + ' 1x,' + logo2x + ' 2x'}
-              width="374"
-              height="381"
+          <div className="position-logo" />
+        </div>
+        <div className="circle-gray">
+          <div className="button-wrapper">
+            <input
+              className="input-email"
+              value={loginInfo?.email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                console.log(loginInfo.email);
+                setLoginInfo({ ...loginInfo, email: e.target.value });
+              }}
+              placeholder="E-mail"
             />
+            <input
+              className="input-password"
+              value={loginInfo?.password}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setLoginInfo({ ...loginInfo, password: e.target.value });
+              }}
+              placeholder="Password"
+            />
+            <button className="button-login">
+              <p style={{ color: '#fff' }}>Sign In</p>
+            </button>
+            <button className="button-signin">
+              <p>Sign Up</p>
+            </button>
+            <a className="a-password" href="">
+              <p>{'비밀번호를 잊으셨습니까?'}</p>
+            </a>
           </div>
         </div>
-        <div className="circle-gray"></div>
       </div>
     </section>
   );
