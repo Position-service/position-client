a08d9afa56c897feb91fb5429872a753e1b420b7
hyemmie <lhm0710@naver.com>
2021-08-02 16:13:51 +0900

    ✨ [feat] logout handler

--- src/modules/calender/Main.tsx
+++ src/modules/calender/Main.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import ServerContext from '../../contexts/ServerContext';
 import MainTemplate from './MainTemplate';
 
 interface Props {}
 
 const Main = (props: Props) => {
-  return <MainTemplate />;
+  const history = useHistory();
+  const { setServer } = useContext(ServerContext);
+
+  const logoutHandler = () => {
+    window.localStorage.removeItem('position-token');
+    setServer('');
+    history.push('/login');
+  };
+  return (
+    <>
+      <MainTemplate logoutHandler={logoutHandler} />
+    </>
+  );
 };
 
 export default Main;
