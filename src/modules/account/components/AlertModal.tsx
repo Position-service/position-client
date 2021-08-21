ef955552a2591cc73de84cbf653302a090ba3527
hyemmie <lhm0710@naver.com>
2021-08-21 22:11:50 +0900

    add background props at alert modal

--- src/modules/account/components/AlertModal.tsx
+++ src/modules/account/components/AlertModal.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import '../css/AlertModal.css';
 
 interface Props {
+  backgroundColor: string;
   buttonHandler: () => void;
   children?: React.ReactNode;
 }
@@ -9,8 +10,10 @@ interface Props {
 const AlertModal = (props: Props) => {
   return (
     <div className="modal-background">
-      <div className="section-modal">
-        <div className="position-logo-green" />
+      <div
+        className="section-modal"
+        style={{ backgroundColor: props.backgroundColor }}
+      >
         {props.children}
         <button className="modal-confirm-button" onClick={props.buttonHandler}>
           확인
