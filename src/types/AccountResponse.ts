56856994c4c27423be11474983b4d07b5ddb3b5d
hyemmie <lhm0710@naver.com>
2021-09-06 23:50:05 +0900

    add response type

--- src/types/AccountResponse.ts
+++ src/types/AccountResponse.ts
@@ -1,3 +1,6 @@
+import { Schedule } from './Schedule';
+import { TaskGroup } from './Task';
+
 export type AccountResponse = {
   id: string;
   email: string;
@@ -6,4 +9,18 @@ export type AccountResponse = {
 
 export type TokenResponse = {
   token: string;
+};
+
+export type UserDataResponse = {
+  emailAuth: boolean;
+  id: number;
+  email: string;
+  nickName: string;
+  groups: TaskGroup[];
+  schedules: Schedule[];
+};
+
+export type SignInResponse = {
+  token: string;
+  data: UserDataResponse;
 };
